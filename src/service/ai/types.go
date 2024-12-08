81c658a75c6517defdf3afd16a57aeb1e52715bd
rahul-aut-ind <rahul.aut.ind@gmail.com>
2024-12-08 11:11:59 +0100

    refactor

--- src/service/ai/types.go
+++ src/service/ai/types.go
@@ -1,39 +1,6 @@
 package ai
 
-import "time"
-
 type (
-	ImageProcessor struct {
-		IsDeleted      bool          `json:"isDeleted" validate:"required"`
-		UserID         string        `json:"userId" validate:"required"`
-		PhotoID        string        `json:"photoId" validate:"required"`
-		Path           string        `json:"path" validate:"required"`
-		ContextName    *string       `json:"contextName,omitempty"`
-		ContextID      *string       `json:"contextId,omitempty"`
-		OpenApiMessage *AnalysisResp `json:"openApiMessage,omitempty"`
-		GeminiMessage  *AnalysisResp `json:"geminiMessage,omitempty"`
-		TakenAt        time.Time     `json:"takenAt" validate:"required"`
-		UploadedAt     time.Time     `json:"uploadedAt" validate:"required"`
-		UpdatedAt      time.Time     `json:"updatedAt" validate:"required"`
-		EncodedImage   string        `json:"encodedImage" validate:"required"`
-	}
-
-	ImageProcessorResp struct {
-		ID          string `json:"id"`
-		FullSizeURL string `json:"fullSizeUrl"`
-		Metadata
-	}
-
-	Metadata struct {
-		TakenAt time.Time        `json:"takenAt" validate:"required"`
-		Context *MetadataContext `json:"context,omitempty"`
-	}
-
-	MetadataContext struct {
-		ID   *string `json:"id,omitempty"`
-		Name string  `json:"name" validate:"required"`
-	}
-
 	AnalysisResp struct {
 		ID                        string           `json:"id"`
 		ImageURL                  string           `json:"fullSizeUrl"`
