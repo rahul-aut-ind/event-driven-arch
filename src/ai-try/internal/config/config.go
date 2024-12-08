cadd62defda921d804cdf27047ed14582950d80e
rahul-aut-ind <rahul.aut.ind@gmail.com>
2024-12-08 11:17:03 +0100

    reafactoring

--- src/ai-try/internal/config/config.go
+++ src/ai-try/internal/config/config.go
@@ -25,8 +25,6 @@ type (
 		S3Bucket string
 		// S3Directory the directory in the s3 bucket
 		S3Directory string
-		// S3ScannerDirectory the directory in the s3 bucket for meal scanner
-		S3ScannerDirectory string
 		// VisionAPIKey the api key for OpenAI vision API
 		VisionAPIKey string
 		// GeminiAPIKey the api key for OpenAI vision API
@@ -37,25 +35,12 @@ type (
 		CDNUrls string
 		// SecretName the name of the secret in secrets manager
 		SecretName string
-		// EventEncryption the name of the secret in secrets manager
-		EventEncryption string
-	}
-	eventEncryption struct {
-		Key string `json:"key"`
 	}
 )
 
 const (
 	// EnvironmentDevelopment development environment
 	EnvironmentDevelopment = "development"
-	// HeaderUserID name of the header that holds the user id
-	HeaderUserID = "x-user-id"
-	// HeaderIDToken name of the header that holds the id token
-	HeaderIDToken = "x-id-token"
-	// HeaderUserLocale name of the header that holds the user locale
-	HeaderUserLocale = "x-user-locale"
-	// HeaderUserGender name of the header that holds the user gender
-	HeaderUserGender = "x-user-gender"
 )
 
 // NewEnv creates a new instance of Env
@@ -71,18 +56,16 @@ func NewEnv() *Env {
 	}
 
 	return &Env{
-		Environment:        os.Getenv("ENVIRONMENT"),
-		DefaultAWSRegion:   os.Getenv("AWS_REGION"),
-		TableName:          os.Getenv("DYNAMODB_TABLE_NAME_SERVICE"),
-		LogLevel:           os.Getenv("LOG_LEVEL"),
-		S3Bucket:           os.Getenv("S3_BUCKET_NAME"),
-		S3Directory:        os.Getenv("S3_DIRECTORY"),
-		S3ScannerDirectory: os.Getenv("S3_SCANNER_DIRECTORY"),
-		VisionAPIKey:       os.Getenv("OPENAI_VISION_API_KEY"),
-		GeminiAPIKey:       os.Getenv("GEMINI_API_KEY"),
-		CFSignKey:          os.Getenv("CLOUDFRONT_PUBLIC_KEY_ID"),
-		CDNUrls:            os.Getenv("CDN_URLS"),
-		SecretName:         os.Getenv("SECRET_NAME"),
-		EventEncryption:    os.Getenv("EVENT_ENCRYPTION_SECRET_NAME"),
+		Environment:      os.Getenv("ENVIRONMENT"),
+		DefaultAWSRegion: os.Getenv("AWS_REGION"),
+		TableName:        os.Getenv("DYNAMODB_TABLE_NAME_SERVICE"),
+		LogLevel:         os.Getenv("LOG_LEVEL"),
+		S3Bucket:         os.Getenv("S3_BUCKET_NAME"),
+		S3Directory:      os.Getenv("S3_DIRECTORY"),
+		VisionAPIKey:     os.Getenv("OPENAI_VISION_API_KEY"),
+		GeminiAPIKey:     os.Getenv("GEMINI_API_KEY"),
+		CFSignKey:        os.Getenv("CLOUDFRONT_PUBLIC_KEY_ID"),
+		CDNUrls:          os.Getenv("CDN_URLS"),
+		SecretName:       os.Getenv("SECRET_NAME"),
 	}
 }
